7bb83421395d0ede529b46fa7e1a03abe2bb9395
kalapacshami <126401698+kalapacshami@users.noreply.github.com>
2023-04-11 22:51:43 +0200

    korte added

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -7,6 +7,8 @@ namespace HelloGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //alma
+            //körte
         }
     }
 }
